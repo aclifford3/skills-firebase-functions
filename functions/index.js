72dbe91905ec89c1f0cbcb114dc5e288014b4a69
vkp43001 <pvivek148@gmail.com>
2018-07-18 15:08:43 -0400

    Added Function to index
    added function to get user based on major

--- functions/index.js
+++ functions/index.js
@@ -27,3 +27,20 @@ exports.getUserByName = functions.https.onRequest((req, res) => {
             throw new Error(err);
     });
 });
+
+exports.getUserByMajor = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    // const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    var peopleRef = db.collection('people').doc(req.body.major);
+    peopleRef.get()
+        .then(doc => {
+            console.log('Doc is ', doc.data());
+            return res.status(200).json(doc.data());
+        })
+        .catch(err => {
+            console.log('Oops! Something went wrong.');
+            throw new Error(err)
+        });
+
+});
